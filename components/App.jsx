244dd7bf1642832a09610898b26bd35c9698af37
andrew-heath <aheath42@gmail.com>
2016-07-11 15:39:57 -0700

    Add starter calls for page modules, outline monster information format

--- components/App.jsx
+++ components/App.jsx
@@ -7,16 +7,42 @@ class App extends React.Component {
       currentScore: 0,
       totalScore: 0,
       clickPerSecond: 0,
-      clickPerClick: 1
+      clickPerClick: 1,
+      currentMonster: {},
+      monsterList: {
+        // Move monster list to a database?
+        // monName: {
+        //   name: '',
+        //   img: filepath,
+        //   clicks: {
+        //     curr: 0,
+        //     need: null
+        //   },
+        //   points: null}
+      }
     }
     this.clickUpgrade = this.clickUpgrade.bind(this);
     this.clickTarget = this.clickTarget.bind(this);
     this.clickMonster = this.clickMonster.bind(this);
+    this.increasePoints = this.increasePoints.bind(this);
   }
 
   render() {
     return (
       // html format here
+      <div>
+        <Score currScore={this.state.currentScore}
+          totalScore={this.state.totalScore} />
+        <Upgrades currScore={this.state.currentScore}
+          clkPerSec={this.state.clickPerSecond}
+          clkPerClk={this.state.clickPerClick}
+          click={this.clickUpgrade.bind(this)} />
+        <Arena state={this.state}
+          click={this.clickTarget.bind(this)} />
+        <Monsters clkPerSec={this.state.clickPerSecond}
+          clkPerClk={this.state.clickPerSecond}
+          currMonster={this.state.currentMonster}
+      </div>
     );
   }
 }
