f503e873e7e315b7cfc3fb145e02b8856cbe1eb7
andrew-heath <aheath42@gmail.com>
2016-07-11 20:14:13 -0700

    Remove extraneous comments, remove possible problem code

--- components/Monsters.jsx
+++ components/Monsters.jsx
@@ -2,9 +2,7 @@ var Monsters = ({monsterList, totalPoints, click}) => (
   <div>
     <h3>Monsters</h3>
     {_.map(monsterList, monster => {
-      if (monster.unlock <= totalPoints) {
-        <MonsterLVE monster={monster} click={click} />
-      }
+      <MonsterLVE monster={monster} click={click} />
     })}
   </div>
 );
--- components/Upgrades.jsx
+++ components/Upgrades.jsx
@@ -1,12 +1,10 @@
 var Upgrades = ({clickPower, upgradeList, click}) => (
-  // generate list of all
   <div>
-  // can JS within brackets
     <h3>Click Power:</h3> {clickPower}<br />
     <h2>Upgrades</h2>
     {_.map(upgradeList, upgrade => 
       <UpgradeLVE upgrade={upgrade} click={click} />
-    )};
+    )}
   </div>
 );
 
