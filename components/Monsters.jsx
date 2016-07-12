4518c1a0066f50393107e26019a66476d453f49a
andrew-heath <aheath42@gmail.com>
2016-07-11 20:18:50 -0700

    fix and organize code to function for the better

--- components/Monsters.jsx
+++ components/Monsters.jsx
@@ -2,7 +2,9 @@ var Monsters = ({monsterList, totalPoints, click}) => (
   <div>
     <h3>Monsters</h3>
     {_.map(monsterList, monster => {
-      <MonsterLVE monster={monster} click={click} />
+      if (monster.unlock <= totalPoints) {
+        <MonsterLVE monster={monster} click={click} />
+      }
     })}
   </div>
 );
